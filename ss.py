7b30e5780dae929aa5ea667437c7d859f7f66b30
Evgeniy Gordeev <89735790+evgeniy-gordeev@users.noreply.github.com>
2024-05-15 15:02:30 +0300

    title

--- ss.py
+++ ss.py
@@ -2,8 +2,7 @@ import streamlit as st
 from funcs import *
 
 st.set_page_config(
-     page_title='Crypto portfolio analysis',
-     page_icon = '📈',
+     page_title='Crypto advanced analytics'
 )
 
 import pandas as pd
